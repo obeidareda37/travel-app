9864091bc18abd261d3ea0144811178682e5c688
Obeida Raida <70585602+obeidareda37@users.noreply.github.com>
2023-07-02 20:43:03 +0300

    Route Transitions

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <TheNavigation />
   <div class="container">
-    <router-view :key="$route.path" />
+    <router-view v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <component :is="Component" :key="$route.path"></component>
+      </transition>
+    </router-view>
   </div>
 </template>
 <script>
@@ -11,3 +15,24 @@ export default {
   components: { TheNavigation },
 };
 </script>
+<style lang="css">
+/* name slide */
+/* .slide-enter-active,
+.slide-leave-to {
+  transition: opacity 1s, transform 1s;
+}
+.slide-enter-from,
+.slide-leave-to {
+  opacity: 0;
+  transform: translateX(-30%);
+} */
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
